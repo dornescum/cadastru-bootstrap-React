e165378f44f0115de107515a765759a8c56944eb
Mihai Dornescu <dornescu@gmail.com>
2021-10-13 10:59:48 +0300

    test testimonials

--- src/setup/subcomponents/testimonials.js
+++ src/setup/subcomponents/testimonials.js
@@ -14,7 +14,7 @@ export const Testimonials = [
     {
         id: 102,
         text: ' Buna ziua, Vreau sa spun, ca am ramas placut surprinsa ,cand dupa nici 10 minute dupa ce am facut acea cerere, am inceput sa primesc raspunsuri, am putut sa cer detalii si mi s-a raspuns imediat. Totul a decurs rapid, profesionist, si s-a rezolvat nespus de repede. Multumesc si eu pentru serviciile bune pe care le dati, va doresc mult succes, si nu coborati stacheta. Felicitari!',
-        nume: 'Ginia.B',
+        nume: 'Ginia.c',
         data: '01.01.2020'
     },
 ]
